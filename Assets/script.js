e4041863455d822b338b4b5698c7dded260e2bed
TScar <tscarlett@live.com.au>
2023-03-20 16:12:02 +1100

    HTML is mostly finished. JS is halfway there

--- Assets/script.js
+++ Assets/script.js
@@ -1,29 +1,86 @@
-// 1. Listen for form input events.
-//     a. When the form is submitted, prevent the default form submission behavior.
-//     b. Get the value of the city input field.
+var SearchBtnEl = $("#search-btn");
+var citySearchEl = $("#city-input");
+var daysEl = $(".5-days");
+var form = $("#search-form");
+var apiKey = "70554f79fa035299cd5c105d81410758";
+var countryCode = "AU";
 
-handleSubmitForm function(){
-    preventDefault()
+citySearchEl("input", (event) => {
+    event.preventDefault()
 
-    
-}
+    var formData = new FormData(event.target);
+    var city = formData.get("city")
 
+    var url = `http://api.openweathermap.org/data/2.5/forecast?q=${city},${countryCode}&appid=${apiKey}`;
 
+    fetch(url)
+        .then(response => {
 
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
 
+            return response.json();
+        })
 
+        .then(data => {
+            console.log(data);
 
+        
+    var cardEl = $(".card");
+    cardEl.find(".card-title").text(data.city.name)//dayjs()?
+    cardEl.find(".card-text-temp").text(`temp: ${data.list[0].main.temp}`);
+    cardEl.find(".card-text-wind").text(`Wind ${data.list[0].main.wind}`);
+    cardEl.find(".card-text-humidity").text(`humidity ${data.list[0].main.humidity}`);
 
+    for (let i = 0; i < 5; i++) {
+        var listItem = $(`#day-${i + 1}`);
+        var weatherData = data.list[i];
+        var temperature = weatherData.main.temp;
+        var humidity = weatherData.main.humidity;
+        var windSpeed = weatherData.main.wind.speed;
+        listItem.innerHTML = `Temperature: ${temperature}, Humidity: ${humidity}, Wind Speed: ${windSpeed}`;
+    }
+})
+.catch (error => {
+    console.error('Error:', error);
+});
+      
+});
 
-// 2. Call the weather API to fetch the weather data for the city.
-//     a. Construct the API request URL with the city name and API key.
-//     b. Make a fetch request to the API endpoint with the constructed URL.
-//     c. Handle any errors that may occur during the request.
+$(".list-group-item").on("click", function (event) {
+    event.preventDefault();
+
+    var city = $(this).text();
+    citySearchEl.val(city);
+    form.submit();
+});
+
 // 3. Parse the API response data.
 //     a. Extract the necessary data from the API response, such as the current weather conditions, temperature, humidity, and wind speed.
-// 4. Update the UI with the weather data for the selected city.
+
+// var parseApiResponse = function(){
+//     fetch(url)
+//     .then(function (response){
+//         if (response.ok){
+//             response.json().then(function (data){
+//                 displayWeather(data.items);//function below
+//             });
+//         } else {
+//             alert("error" + response.statusText);
+//         }
+//     });
+// };
+
+
+
+// 4. Update the UI with the weather data for the selected city
 //     a. Display the city name, date, weather icon, temperature, humidity, and wind speed for the current weather conditions.
 //     b. Display a 5-day forecast that shows the date, weather icon, temperature, wind speed, and humidity for each day.
+
+//displayWeather (function){}
+
+
 // 5. Add the selected city to the search history.
 //     a. Store the city name in an array or database that keeps track of all previous searches.
 // 6. Listen for click events on the search history items.
